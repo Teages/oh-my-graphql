0bcb4886c74ab623786fb8ce77fed9826479d407
Teages <Teages@teages.xyz>
2026-04-19 15:56:57 +0800

    fix: disable lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   rules: {
-    curly: ['error', 'all'],
+    'curly': ['error', 'all'],
+    'eslint-comments/no-unlimited-disable': 'off',
   },
 })
